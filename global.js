475f03e482a03b2afd01b8255ab0de18d2839cab
Becky Soll <beckysoll@gmail.com>
2023-04-17 16:21:38 -0400

    today's edits

--- global.js
+++ global.js
@@ -22,8 +22,51 @@
   
     $('#menu-toggle').click(function(){
       $(this).toggleClass('open');
-      $('#block-betterbus-main-menu').slideToggle();
+      if ($('#block-betterbus-main-menu').length > 0) {
+        $('#block-betterbus-main-menu').slideToggle();
+      } else if ($('#block-spanishnavigation').length > 0) {
+        $('#block-spanishnavigation').slideToggle();
+      }
+
     })
+
+    $('#search-map-table').keyup(function() {
+      // var input;
+      // var td;
+      // var i;
+      // var txtValue;
+      var tableQuery = this.value.toLowerCase().replace(/\s+/g, '');
+
+      var table = $('.map-tables .table-group table');
+      var tr = table.find('tr:contains('+tableQuery+')');
+      window.console.log(tr);
+      
+      // Loop through all table rows, and hide those who don't match the search query
+      // for (i = 0; i < tr.length; i++) {
+      //   window.console.log($('.map-tables .table-group table tr:eq('+i+')').find('td'));
+      //   td = tr[i].getElementsByTagName("td")[0];
+      //   // window.console.log(td);
+      //   $('.map-tables .table-group table tr:eq('+i+')').find('td').each(function() {
+      //     // window.console.log(this.textContent);
+      //     txtValue = this.textContent || this.innerText;
+      //     if (txtValue.toLowerCase().indexOf(tableQuery) > -1) {
+      //       tr[i].style.display = "";
+      //     } else {
+      //       tr[i].style.display = "none";
+      //     }
+      //   })
+
+        // if (td) {
+        //   txtValue = td.textContent || td.innerText;
+        //   if (txtValue.toLowerCase().indexOf(tableQuery) > -1) {
+        //     tr[i].style.display = "";
+        //   } else {
+        //     tr[i].style.display = "none";
+        //   }
+        // }
+      // }
+    });
+    
 
   });
 
